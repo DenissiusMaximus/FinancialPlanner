0b126f093eb76296589a2f68e68c737a9cdeef66
DenissiusMaxximus <deniskramar07@gmail.com>
2026-04-06 00:31:18 +0300

    Add query extension methods for filtering and sorting Aims and PlannedTransactions

---
+++ API/API/Extensions/EF/PlannedTransactionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using API.Models;
+
+namespace API.Extensions;
+
+public static class PlannedTransactionExtensions
+{
+    public static IQueryable<PlannedTransaction> FilterByAmount(this IQueryable<PlannedTransaction> query, decimal? minimalAmount, decimal? maximalAmount)
+    {
+        if (minimalAmount.HasValue)
+            query = query.Where(a => a.Amount >= minimalAmount.Value);
+
+        if (maximalAmount.HasValue)
+            query = query.Where(a => a.Amount <= maximalAmount.Value);
+
+        return query;
+    }
+
+    public static IQueryable<PlannedTransaction> ApplySorting(this IQueryable<PlannedTransaction> query, bool descending)
+    {
+        return descending
+            ? query.OrderByDescending(a => a.Amount)
+            : query.OrderBy(a => a.Amount);
+    }
+}
